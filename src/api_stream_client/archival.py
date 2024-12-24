fe9c12bb317fd48269ae46c22634ce8fb911f092
Andrew Dickinson <andrewjd@amazon.com>
2024-12-23 23:40:30 -0500

    Add current data JSON as well as historical

--- src/api_stream_client/archival.py
+++ src/api_stream_client/archival.py
@@ -13,10 +13,17 @@ def archive_event_message(event: dict, path_prefix: str):
 
 def archive_db_snapshot(api_payload: dict, path_prefix: str):
     api_snapshot_time = datetime.fromtimestamp(api_payload["updated"] / 1000)
-    file_path = os.path.join(
+    archival_file_path = os.path.join(
         path_prefix,
         api_snapshot_time.strftime("%Y/%m/%d/%H/%M") + ".json",
     )
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    with open(file_path, "w") as f:
+    os.makedirs(os.path.dirname(archival_file_path), exist_ok=True)
+    with open(archival_file_path, "w") as f:
+        f.write(json.dumps(api_payload))
+
+    current_file_path = os.path.join(
+        path_prefix,
+        "../../mesh_ospf_data.json",
+    )
+    with open(current_file_path, "w") as f:
         f.write(json.dumps(api_payload))
